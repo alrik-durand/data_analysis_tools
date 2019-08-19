3eb8a1b1704f6c8bf0a92195485443c5439059d7
Alrik <alrik.durand@gmail.com>
2019-08-19 19:00:49 +0200

    added gradient fill helper methods

--- plot_tools.py
+++ plot_tools.py
@@ -3,6 +3,8 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.patches import Polygon
 import data_analysis_tools.data as dat
 import pandas as pd
 
@@ -15,6 +17,7 @@ def set_rc_params(mpl):
     mpl.rcParams['legend.fontsize'] = 13
     mpl.rcParams['xtick.labelsize'] = 13
     mpl.rcParams['ytick.labelsize'] = 13
+    mpl.rcParams['figure.facecolor'] = (0, 0, 0, 0)
 
 
 def plot_simple_data(x, y, ax=None, **kw):
@@ -211,7 +214,10 @@ def plot_data(ax, df, rebin_ratio=1, colors=None, cmap=None, window=None, x='x',
             plot_kw_row = plot_kw.copy()
             if 'plot_kw' in row.keys() and isinstance(row['plot_kw'], dict):
                 plot_kw_row.update(row['plot_kw'])
-            line = ax.plot(x_data, y_data+j*offset_increment, label=label_row, color=color, **plot_kw_row)
+            if 'color' in plot_kw:
+                line = ax.plot(x_data, y_data + j * offset_increment, label=label_row, **plot_kw_row)
+            else:
+                line = ax.plot(x_data, y_data + j * offset_increment, label=label_row, color=color, **plot_kw_row)
             j += 1
             lines.append(line)
     return lines
@@ -297,3 +303,103 @@ def new_figure(xlabel=None, ylabel=None, figsize=None):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     return fig, ax
+
+
+def gradient_fill(x, y, fill_color=None, ax=None, **kwargs):
+    """
+    Plot a line with a linear alpha gradient filled beneath it.
+
+    Parameters
+    ----------
+    x, y : array-like
+        The data values of the line.
+    fill_color : a matplotlib color specifier (string, tuple) or None
+        The color for the fill. If None, the color of the line will be used.
+    ax : a matplotlib Axes instance
+        The axes to plot on. If None, the current pyplot axes will be used.
+    Additional arguments are passed on to matplotlib's ``plot`` function.
+
+    Returns
+    -------
+    line : a Line2D instance
+        The line plotted.
+    im : an AxesImage instance
+        The transparent gradient clipped to just the area beneath the curve.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    line, = ax.plot(x, y, **kwargs)
+    if fill_color is None:
+        fill_color = line.get_color()
+
+    zorder = line.get_zorder()
+    alpha = line.get_alpha()
+    alpha = 1.0 if alpha is None else alpha
+
+    z = np.empty((100, 1, 4), dtype=float)
+    rgb = mcolors.colorConverter.to_rgb(fill_color)
+    z[:,:,:3] = rgb
+    z[:,:,-1] = np.linspace(0, alpha, 100)[:,None]
+
+    xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
+    im = ax.imshow(z, aspect='auto', extent=[xmin, xmax, ymin, ymax],
+                   origin='lower', zorder=zorder)
+
+    xy = np.column_stack([x, y])
+    xy = np.vstack([[xmin, ymin], xy, [xmax, ymin], [xmin, ymin]])
+    clip_path = Polygon(xy, facecolor='none', edgecolor='none', closed=True)
+    ax.add_patch(clip_path)
+    im.set_clip_path(clip_path)
+
+    ax.autoscale(True)
+    return line, im
+
+
+def gradient_fill_exp(x, y, fill_color=None, ax=None, **kwargs):
+    """
+    Plot a line with a linear alpha gradient filled beneath it.
+
+    Parameters
+    ----------
+    x, y : array-like
+        The data values of the line.
+    fill_color : a matplotlib color specifier (string, tuple) or None
+        The color for the fill. If None, the color of the line will be used.
+    ax : a matplotlib Axes instance
+        The axes to plot on. If None, the current pyplot axes will be used.
+    Additional arguments are passed on to matplotlib's ``plot`` function.
+
+    Returns
+    -------
+    line : a Line2D instance
+        The line plotted.
+    im : an AxesImage instance
+        The transparent gradient clipped to just the area beneath the curve.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    line, = ax.plot(x, y, **kwargs)
+    if fill_color is None:
+        fill_color = line.get_color()
+
+    zorder = line.get_zorder()
+
+    z = np.empty((100, 1, 4), dtype=float)
+    rgb = mcolors.colorConverter.to_rgb(fill_color)
+    z[:,:,:3] = rgb
+    z[:,:,-1] = 1-np.exp(-np.linspace(0, 6, 100)[:,None])
+
+    xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
+    im = ax.imshow(z, aspect='auto', extent=[xmin, xmax, ymin, ymax],
+                   origin='lower', zorder=zorder)
+
+    xy = np.column_stack([x, y])
+    xy = np.vstack([[xmin, ymin], xy, [xmax, ymin], [xmin, ymin]])
+    clip_path = Polygon(xy, facecolor='none', edgecolor='none', closed=True)
+    ax.add_patch(clip_path)
+    im.set_clip_path(clip_path)
+
+    ax.autoscale(True)
+    return line, im
